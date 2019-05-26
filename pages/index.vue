005275b7d8f99952601fea402a2321b223892175
Mitsui <todays.mitsui@gmail.com>
2019-05-27 00:19:19 +0900

    feat: name, bio 表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,39 @@
 <template>
   <section class="section">
-    <div class="columns is-mobile">
-      <card title="Free" icon="github-circle">
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub</a>
-      </card>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-one-quarter">
+              <img :src="avatarUrl" :alt="login" />
+            </div>
 
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey">Every</b> component is responsive
-      </card>
-
-      <card title="Modern" icon="alert-decagram">
-        Built with <a href="https://vuejs.org/">Vue.js</a> and
-        <a href="http://bulma.io/">Bulma</a>
-      </card>
-
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
-    </div>
+            <div class="column">
+              <h1 class="title">{{ name }}</h1>
+              <h2 class="subtitle">{{ login }}</h2>
+              <article v-html="bioHTML"></article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </section>
 </template>
 
 <script>
-import Card from '~/components/Card'
+import { mapState, mapActions } from 'vuex'
 
 export default {
-  name: 'HomePage',
+  computed: {
+    ...mapState(['login', 'name', 'avatarUrl', 'bio', 'bioHTML'])
+  },
 
-  components: {
-    Card
+  mounted() {
+    this.fetchUserData()
+  },
+
+  methods: {
+    ...mapActions(['fetchUserData'])
   }
 }
 </script>
